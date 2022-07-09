8b113de0725c8cf5ef2ef6fc8ce46431caed5e79
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2022-07-09 09:30:27 +0100

    Dont work|apply style

--- src/components/graficComponents.js
+++ src/components/graficComponents.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core'
 import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
   binaryIn: {
-    width: '20px',
-    height: '20px',
+    padding: '5px',
     backgroundColor: 'green',
     margin: '3px',
     textAlign: 'center'
   },
   binaryOut: {
-    width: '20px',
-    height: '20px',
+    padding: '5px',
     backgroundColor: 'red',
     margin: '3px',
     textAlign: 'center'
@@ -32,6 +30,10 @@ const GraficComponent = (props) => {
   // console.log("Variable Index: " + selectVariable);
   // console.log("Variable Valor: ", variableValue);
 
+  useEffect(() => {
+    console.log("Reload Page");
+  }, [attrStyle])
+
   if(variableValue.shape.type === "vector" &&
      variableValue.range.lowerBound === 0 && 
      variableValue.range.upperBound === 1) {
@@ -43,15 +45,35 @@ const GraficComponent = (props) => {
         <Grid item >Simbolo de la Variable: {variableValue.symbol}</Grid>
         
         <Grid item container justifyContent='center'>
-          {variableValue.value.map((value, index) => {
-            return (
-              <Grid item
-                className={value === 0 ? classes.binaryOut : classes.binaryIn}
-              >
-                {index + 1}
-              </Grid>
-            )
-          })}
+          {
+            problem.problem.objects.map((values, index) => {
+              const isInside = variableValue.value[index]
+              const attributes = {}
+              values.attributes.forEach(value => {
+                attributes[value.attribute] = value.value
+              })
+              
+              const useWidth = attrStyle['width'] === 'default' ? '' : attributes[attrStyle['width']] + "px !important"
+              const useHeight = attrStyle['height'] === 'default' ? '' : attributes[attrStyle['height']] + "px !important"
+              const useColor = attrStyle['color'] === 'default' ? '' : attributes[attrStyle['color']]
+              console.log("useWidth:", useWidth);
+              console.log("useHeight:", useHeight);
+              console.log("useColor:", useColor);
+
+              return (
+                <Grid item
+                  className={isInside === 0 ? classes.binaryOut : classes.binaryIn}
+                  style={{
+                    width: {useWidth},
+                    height: {useHeight},
+                    color: {useColor},
+                  }}
+                >
+                  {attributes.name}
+                </Grid>
+              )
+            })
+          }
         </Grid>
       </Grid>
     )
